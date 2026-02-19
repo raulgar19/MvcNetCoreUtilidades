8f40f2896531afcbc6fbf35ca7ca1a7724f7d743
raulgar19 <raulgamu2003@gmail.com>
2026-02-19 14:19:30 +0100

    Add project files.

---
+++ MvcNetCoreUtilidades/Controllers/UploadFilesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCoreUtilidades.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        private IWebHostEnvironment hostEnvironment;
+
+        public UploadFilesController(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult SubirFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubirFile(IFormFile fichero)
+        {
+            string rootFolder = this.hostEnvironment.WebRootPath;
+            string fileName = fichero.FileName;
+            string path = Path.Combine(rootFolder, "uploads", fileName);
+
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                await fichero.CopyToAsync(stream); 
+            }
+
+            ViewData["MENSAJE"] = "Fichero subido a " + path;
+            ViewData["FILENAME"] = fileName;
+
+            return View();
+        }
+    }
+}
