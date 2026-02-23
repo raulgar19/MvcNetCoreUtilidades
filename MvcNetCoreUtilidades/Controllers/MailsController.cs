6dc9cddb92f5dc99f5ff20a29cf916952411c9ea
raulgar19 <raulgamu2003@gmail.com>
2026-02-23 12:58:15 +0100

    Add email sending and encryption utilities with UI
    Added MailsController and views for sending emails via SMTP, with configuration in appsettings.json. Introduced CifradosController and HelperCryptography for basic (SHA1) and efficient (SHA512 with salt) encryption features, along with corresponding views. Updated navigation to include links to new mail and encryption utilities. Added Microsoft.Extensions.Caching.Memory package.

---
+++ MvcNetCoreUtilidades/Controllers/MailsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MvcNetCoreUtilidades.Controllers
+{
+    public class MailsController : Controller
+    {
+        private IConfiguration configuration;
+
+        public MailsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMail(string to, string asunto, string mensaje)
+        {
+            string user = this.configuration.GetValue<string>("MailSettings:Credentials:User");
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress(user);
+            mail.To.Add(to);
+            mail.Subject = asunto;
+            mail.Body = mensaje;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.Normal;
+
+            string password = this.configuration.GetValue<string>("MailSettings:Credentials:Password");
+            string host = this.configuration.GetValue<string>("MailSettings:Server:Host");
+            int port = this.configuration.GetValue<int>("MailSettings:Server:Port");
+            bool ssl = this.configuration.GetValue<bool>("MailSettings:Server:Ssl");
+            bool defaultCredentials = this.configuration.GetValue<bool>("MailSettings:Server:DefaultCredentials");
+
+            SmtpClient client = new SmtpClient();
+            client.Host = host;
+            client.Port = port;
+            client.EnableSsl = ssl;
+            client.UseDefaultCredentials = defaultCredentials;
+            
+            NetworkCredential credentials = new NetworkCredential(user, password);
+            client.Credentials = credentials;
+
+            await client.SendMailAsync(mail);
+            ViewData["MENSAJE"] = "Mensaje enviado correctamente";
+
+            return View();
+        }
+    }
+}
---
+++ MvcNetCoreUtilidades/Helpers/HelperCryptography.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MvcNetCoreUtilidades.Helpers
+{
+    public class HelperCryptography
+    {
+        public static string Salt {  get; set; }
+
+        public static string EncriptarTextoBasico(string contenido)
+        {
+            byte[] entrada;
+            byte[] salida;
+
+            UnicodeEncoding encoding = new UnicodeEncoding();
+
+            SHA1 managed = SHA1.Create();
+
+            entrada = encoding.GetBytes(contenido);
+
+            salida = managed.ComputeHash(entrada);
+
+            string resultado = encoding.GetString(salida);
+
+            return resultado;
+        }
+
+        public static string CifrarContenido(string contenido, bool comparar) 
+        { 
+            if (comparar == false)
+            {
+                Salt = GenerateSalt();
+            }
+
+            string contenidoSalt = contenido + Salt;
+
+            SHA512 managed = SHA512.Create();
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] salida;
+            
+            salida = encoding.GetBytes(contenidoSalt);
+
+            for (int i = 1; i <= 21; i++)
+            {
+                salida = managed.ComputeHash(salida);
+            }
+
+            managed.Clear();
+
+            string resultado = encoding.GetString(salida);
+
+            return resultado;
+        }
+
+        private static string GenerateSalt()
+        {
+            Random random = new Random();
+            string salt = "";
+            
+            for (int i = 1; i < 30; i++)
+            {
+                int num = random.Next(1, 255);
+                char letra = Convert.ToChar(num);
+                salt += letra;
+            }
+
+            return salt;
+        }
+    }
+}
